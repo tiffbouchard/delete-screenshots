27f292b6c9d950d36110912807257e388e1bb5fd
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-08-26 14:08:28 -0400

    testing deleting files

---
+++ delete-screenshots.py
@@ -0,0 +1,19 @@
+import os 
+
+# def find_screenshots():
+
+print(os.getcwd())
+
+print(os.remove("/Users/tiffanybouchard/Desktop/unnamed.png"))
+
+# for dirpath, dirnames, files in os.walk('.'):
+#     print(f'Found directory: {dirpath}')
+#     for file_name in files:
+#         print(file_name)
+
+
+
+
+
+
+#         //take all photos and put them into a photos folder
